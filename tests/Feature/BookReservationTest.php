37c45fc0f9c7f9b55736369ddbe8bd27721234ba
Gonzalo Di Berardino <gonzadiber@gmail.com>
2019-05-05 22:52:52 -0300

    adds Book resourses

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,20 +2,66 @@
 
 namespace Tests\Feature;
 
+use App\Book;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => 'Cool Book tittle',
+            'author' => 'Gonza',
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Gonza',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books', [
+            'title' => 'Cool title',
+            'author' => 'Gonza',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/'.$book->id,[
+            'title' => 'New title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
     }
 }
